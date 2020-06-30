0554b95b53a43bdac63cdc50ed520548dfe40d15
prakhar-dhakar <37078245+prakhar-dhakar@users.noreply.github.com>
2020-06-30 19:57:05 +0530

    Error Removal
    Removed error when batch_size of remaining was less than required batch_size

--- Custom_Image_Generator.py
+++ Custom_Image_Generator.py
@@ -1,7 +1,7 @@
 import matplotlib.image as mpimg
 from keras.utils import to_categorical
  
-def generator(dataframe, x_col, y_col, directory, batch_size=32,target_size = (224,224)):
+def generator(samples, directory, batch_size=32,target_size = (224,224)):
 
     def resize_img_keeping_aspect(image,target_size):
         # print(image.shape)
@@ -19,24 +19,15 @@ def generator(dataframe, x_col, y_col, directory, batch_size=32,target_size = (2
         image = tf.image.random_flip_left_right(image)
         return image/255.0
 
-    def load_samples(dataframe,x_col,y_col):
-        file_names = list(dataframe[x_col])
-        
-        # Get the labels present in the second column
-        labels = list(dataframe[y_col])
-        labels = to_categorical(labels)
-        samples=[]
-        for samp,lab in zip(file_names,labels):
-            samples.append([samp,lab])
-        return samples
 
     """
     Yields the next training batch.
     Suppose `samples` is an array [[image1_filename,label1], [image2_filename,label2],...].
     """
-    samples = load_samples(dataframe,x_col,y_col)
     num_samples = len(samples)
     input_x, input_y = target_size
+    num_steps = num_samples/batch_size
+    num_labels = samples[0][1].shape[0]
 
     while True: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -45,10 +36,10 @@ def generator(dataframe, x_col, y_col, directory, batch_size=32,target_size = (2
         for offset in range(0, num_samples, batch_size):
             # Get the samples you'll use in this batch
             batch_samples = samples[offset:offset+batch_size]
- 
             # Initialise X_train and y_train arrays for this batch
-            X_train = np.zeros(shape = (batch_size,input_x,input_y,3))
-            y_train = []
+            this_batch_size = len(batch_samples)
+            X_train = np.zeros(shape = (this_batch_size,input_x,input_y,3))
+            y_train = np.zeros(shape = (this_batch_size,num_labels))
 
             count = 0
  
@@ -62,13 +53,12 @@ def generator(dataframe, x_col, y_col, directory, batch_size=32,target_size = (2
                 y = batch_sample[1]
                 # Add example to arrays
                 X_train[count, :] = image
-                y_train.append(y)
+                y_train[count, :] = y
                 count+=1
  
             # Make sure they're numpy arrays (as opposed to lists)
-
+            # Preprocessing the images as a batch using tf.image
             X_train = preprocess(X_train)
-            y_train = np.array(y_train)
  
             # The generator-y part: yield the next training batch            
             yield X_train.numpy(), y_train
